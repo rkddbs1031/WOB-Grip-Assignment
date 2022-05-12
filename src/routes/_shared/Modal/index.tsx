3cd8e3d78b8521f3d792c1a49a1a35678427ba0a
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-05-12 16:12:43 +0900

    [MOD] Modal Component

--- src/routes/_shared/Modal/index.tsx
+++ src/routes/_shared/Modal/index.tsx
@@ -1,20 +1,28 @@
 import { useSetRecoilState, useRecoilState, useRecoilValue } from 'recoil'
 import { ModalVisible, MovieFavoritList, SelectItem } from 'states/movie'
-import { Favorite2 } from 'assets/svgs/movie'
+import { Favorite3, Favorite2 } from 'assets/svgs/movie'
 
-import styles from 'routes/Movie/Movie.module.scss'
+import styles from 'routes/Routes.module.scss'
+import { useParams } from 'react-router-dom'
 
 
 const Modal = () => {
+  const params = useParams()
 
   const setModalValue = useSetRecoilState(ModalVisible)
   const [ favMovieList, setFavMovieList ] = useRecoilState(MovieFavoritList)
   const selectItem = useRecoilValue(SelectItem)
   
   const handleRemoveFavorite = () => {
-    setFavMovieList([...favMovieList, Object(selectItem)])
-  }    
+    setFavMovieList(favMovieList.filter((item) => item.imdbID !== Object(selectItem).imdbID))
+    setModalValue(false)
+  }
 
+  const handleAddFavorite = () => {
+    setFavMovieList([...favMovieList, Object(selectItem)])
+    setModalValue(false)
+  }    
+  
   const handleRemoveModal = () => {
     setModalValue(false)
   }
@@ -22,10 +30,21 @@ const Modal = () => {
   return( 
     
     <div className={styles.modalWrap}>
-      <button type='button' className={styles.favBtn} onClick={handleRemoveFavorite}>
-        <Favorite2 />
-        <span>즐겨찾기</span>
-      </button>
+      {
+        params.favorites === 'favorites' ? (
+          <button type='button' className={styles.favRemoveBtn} onClick={handleRemoveFavorite}>
+            <Favorite2 />
+            <span>즐겨찾기 취소</span>
+          </button>
+        )
+        : (
+          <button type='button' className={styles.favBtn} onClick={handleAddFavorite}>
+            <Favorite3 />
+            <span>즐겨찾기</span>
+          </button>
+
+        )
+      }
       <button type='button' className={styles.removeBtn} onClick={handleRemoveModal}>취소하기</button>
     </div>
   )
