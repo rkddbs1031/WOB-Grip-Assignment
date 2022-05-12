5bcfc55b5287bce9fce25209802e9da68e9e389f
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-05-12 13:14:40 +0900

    [ADD] Movie data recoil

--- src/states/movie.ts
+++ src/states/movie.ts
@@ -1,5 +1,20 @@
 import { atom } from 'hooks/state'
 
+import { IListItem } from 'types/movie'
+
+export const MovieData = atom<IListItem[]>({
+  key: '#MovieData',
+  default: [
+    {
+     Title: '',
+     Type: '',
+     Year: '',
+     imdbID: '',
+     Poster: ''
+   }
+  ]
+})
+
 export const SearchValue = atom<string>({
   key: '#SearchValue',
   default: ''
