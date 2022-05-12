3f950f397fd5b4155942166eaab62d687a7e4b22
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-05-12 13:55:59 +0900

    [ADD] Favorite movie list recoil

--- src/states/movie.ts
+++ src/states/movie.ts
@@ -1,7 +1,28 @@
 import { atom } from 'hooks/state'
+import { recoilPersist } from 'recoil-persist'
 
 import { IListItem } from 'types/movie'
 
+const { persistAtom } = recoilPersist()
+
+export const MovieFavoritList = atom<IListItem[]>({
+  key: '#MovieFavoritList',
+  default: [],
+  effects_UNSTABLE: [persistAtom], // 새로고침해도 유지
+})
+
+export const SelectItem = atom<IListItem[]>({
+  key: '#SelectItem',
+  default: [
+    {
+     Title: '',
+     Type: '',
+     Year: '',
+     imdbID: '',
+     Poster: ''
+   }
+  ]
+})
 
 export const ModalVisible = atom<Boolean>({
   key: '#ModalVisible',
