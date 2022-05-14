0cf32c04c852e9ffbe2dc850303628cc9a7a68fd
rkddbs1031 <rkddbs1031@gmail.com>
2022-05-14 17:33:49 +0900

    [ADD] searchbox input  focus

--- src/routes/_shared/SearchBox/index.tsx
+++ src/routes/_shared/SearchBox/index.tsx
@@ -1,7 +1,7 @@
-import { ChangeEvent, FormEvent } from 'react'
+import React, { ChangeEvent, FormEvent } from 'react'
 import { useSetRecoilState } from 'recoil'
 
-import { useState } from 'hooks'
+import { useState, useMount } from 'hooks'
 import { SearchValue, PageNum } from 'states/movie'
 
 import styles from 'routes/Movie/Movie.module.scss'
@@ -11,6 +11,7 @@ const SearchBox = () => {
   const setSearchValue = useSetRecoilState(SearchValue)
   const [value, setValue] = useState('')
   const setPage = useSetRecoilState(PageNum)
+  const inputRef = React.useRef<HTMLInputElement | null>(null)
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e?.preventDefault()
@@ -21,9 +22,13 @@ const SearchBox = () => {
 
   const handleSearchKeyword = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
 
+  useMount(() => {
+    inputRef.current?.focus()
+  })
+
   return (
     <form onSubmit={handleSubmit} className={styles.searchBox}>
-      <input type='text' placeholder='search' onChange={handleSearchKeyword} />
+      <input ref={inputRef} type='text' placeholder='search' onChange={handleSearchKeyword} />
       <button type='submit' className={styles.searchBtn}>
         <SearchIcon48 className={styles.submit} />
       </button>
