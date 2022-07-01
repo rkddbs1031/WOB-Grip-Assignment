633aed0012ead8264fd61b46b50a749b4ed51790
rkddbs1031 <rkddbs1031@gmail.com>
2022-07-01 12:03:55 +0900

    MOD: Loading 컴포넌트 분리

---
+++ src/routes/_shared/Loading/index.tsx
@@ -0,0 +1,13 @@
+import styles from './loading.module.scss'
+
+const Loading = () => {
+  return (
+    <div className={styles.loading}>
+      <span />
+      <span />
+      <span />
+    </div>
+  )
+}
+
+export default Loading
